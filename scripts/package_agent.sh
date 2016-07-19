7df377cfd2cd0baed53ad14e865cb1574452c632
richbrowne <r.browne@f5.com>
2016-07-19 10:19:28 -0600

    using script to customize build

--- scripts/package_agent.sh
+++ scripts/package_agent.sh
@@ -1,6 +1,22 @@
 #!/bin/bash -ex
 
+OS_TYPE=$1
+OS_VERSION=$2
+PKG_ROOT=$(pwd)
+DEST_DIR=${PKG_ROOT}/dist
 
-echo "ehllo from the build script"
-pwd
-ls -al
+if [ $OS_TYPE == "redhat" ]; then
+	git clone https://github.com/richbrowne/f5-openstack-agent-rpmdist.git ${DEST_DIR}
+elif [ $OS_TYPE == "ubuntu" ]; then
+	echo "DEBIAN PACKAGING NOT IMPLEMENTED"
+	exit 0
+else
+	echo "Unknown os type: ${OS_TYPE}"
+	exit 1
+fi
+
+BUILD_CONTAINER=${OS_TYPE}${OS_VERSION}-pkg-builder
+BUILD_DIR="/var/bdir"
+docker build -t ${BUILD_CONTAINER} ./${DEST_DIR}/Docker/${OS_TYPE}/${OS_VERSION}
+docker run --privileged -v ${PKG_ROOT}:${BUILD_DIR} ${BUILD_CONTAINER} /bin/bash /build-rpms.sh
+sudo chown -R travis:travis ${DEST_DIR}
